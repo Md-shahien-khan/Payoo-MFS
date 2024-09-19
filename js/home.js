2f1468313afcaf8bab5349a65a8221eb972c3231
Md Shahien Khan <shawonkn58@gmail.com>
2024-09-19 21:56:46 +0100

    added calculation with add money

--- js/home.js
+++ js/home.js
@@ -8,6 +8,25 @@ document.getElementById('btn-add-money').addEventListener('click', function(even
     // step 2 get the amount money we want to add
     const addMoneyInput = document.getElementById('input-add-money').value;
     // step 3 get the amount money we want to add
-    const pinInput = document.getElementById('input-pin-number').value;
+    const pinNumberInput = document.getElementById('input-pin-number').value;
+
+
+    // lets verify the pin (this is not a correct i did because i know the basic only)
+    if(pinNumberInput === '12345'){
+        console.log('addid money to account');
+        // step 5 get the current balance
+        const balance = document.getElementById('account-balance').innerText;
+        // step 6 add add money input with balance 
+        const addMoneyNumber = parseFloat(addMoneyInput);
+        const balanceNumber = parseFloat(balance);
+        const newBalance = addMoneyNumber + balanceNumber;
+        
+        // step 6 update balance in the ui or dom
+        document.getElementById('account-balance').innerText = newBalance;
+    }
+    else{
+        alert('failer to add money please try again');
+    }
+    
     
 })
