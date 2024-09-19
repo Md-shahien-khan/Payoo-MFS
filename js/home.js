c96ea109a487943ab80299b8577f66713363fe14
Md Shahien Khan <shawonkn58@gmail.com>
2024-09-19 21:35:47 +0100

    added event handler in add money

---
+++ js/home.js
@@ -0,0 +1,13 @@
+// add money to the account
+
+// step 1 add event listener to the add money button
+document.getElementById('btn-add-money').addEventListener('click', function(event){
+    // prevent page reload after form submit
+    event.preventDefault();
+
+    // step 2 get the amount money we want to add
+    const addMoneyInput = document.getElementById('input-add-money').value;
+    // step 3 get the amount money we want to add
+    const pinInput = document.getElementById('input-pin-number').value;
+    
+})
