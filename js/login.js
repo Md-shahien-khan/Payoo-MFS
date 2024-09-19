155e74bf3ac342d4a07f2645c762da3c1202314b
Md Shahien Khan <shawonkn58@gmail.com>
2024-09-19 21:14:54 +0100

    Available Amount page done

--- js/login.js
+++ js/login.js
@@ -2,13 +2,21 @@
 document.getElementById('button-login').addEventListener('click', function(event){
     // step 2 prevent default behavior (prevent reloading browser)
     event.preventDefault();
-    console.log('login button clicked');
 
-
-    // step 3 getting the phone number
+    // step 3 getting the phone number and pin number
     const phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber);
+    const pinNumber = document.getElementById('pin-number').value;
+    console.log(phoneNumber, pinNumber);
 
 
+    // step 4 validate the phone and pin
+    // this is a temporary way
+    if(phoneNumber === '12345' && pinNumber === '12345'){
+        console.log('you are logged in');
+        window.location.href = '/home.html';
+    }
+    else{
+        alert('wrong phone number or pin')
+    }
         
 })
